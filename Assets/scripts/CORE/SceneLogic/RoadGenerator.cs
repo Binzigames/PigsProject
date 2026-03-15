922e7a3420b680841a9d17173a09679e9db5c22d
Serhii <ovodsergej@gmail.com>
2026-03-15 20:20:46 +0100

    add core scene logic

---
+++ Assets/scripts/CORE/SceneLogic/RoadGenerator.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class RoadGenerator : MonoBehaviour
+{
+    private IObjectPool<Road> _roadPool;
+    [SerializeField] private List<Road> _roadInPoolList;
+    [SerializeField] private Road _roadPrefab;
+    [SerializeField] private Vector3 _spawnPosition;
+    [SerializeField] private int _defaultPoolCapacity = 5;
+    [SerializeField] private int _maxPoolSize = 10;
+
+    private Road _roadFromPool;
+    private Road _nextRoadToRelease;
+
+    private void Awake()
+    {
+        _roadPool = new ObjectPool<Road>
+        (CreateRoadInstance, OnGetFromPool, OnReleaseToPool, OnDestroyPooledRoad,
+            collectionCheck: true, _defaultPoolCapacity, _maxPoolSize);
+
+        _nextRoadToRelease = _roadInPoolList.FirstOrDefault<Road>();
+        _nextRoadToRelease.RoadPool = _roadPool;
+    }
+    private Road CreateRoadInstance()
+    {
+        Road roadInstance = Instantiate(_roadPrefab, _spawnPosition, Quaternion.identity);
+        roadInstance.RoadPool = _roadPool;
+        return roadInstance;
+    }
+    private void OnGetFromPool(Road pooledRoad)
+    {
+        _roadInPoolList.Add(pooledRoad);
+        _roadFromPool = pooledRoad;
+        pooledRoad.gameObject.SetActive(true);
+    }
+    private void OnReleaseToPool(Road pooledRoad)
+    {
+        _nextRoadToRelease = _roadFromPool;
+        pooledRoad.gameObject.SetActive(false);
+    }
+    private void OnDestroyPooledRoad(Road pooledRoad) // If you hit maximum limit
+    {
+        Destroy(pooledRoad.gameObject);
+    }
+    public void SpawnRoadFromPool()
+    {
+        var roadFromPool = _roadPool.Get();
+        roadFromPool.transform.SetPositionAndRotation(_spawnPosition, Quaternion.identity);
+    }
+    public void ReleaseLastRoad()
+    {
+        _roadPool.Release(_nextRoadToRelease);
+    }
+}
