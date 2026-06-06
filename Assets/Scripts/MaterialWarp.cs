27a8d6ec2df4b0914bbbb8bd5690e773fb2fffa2
Serhii <143748403+SerhiiOvD@users.noreply.github.com>
2026-06-06 17:45:13 +0200

    refactor material warper

--- Assets/Scripts/MaterialWarp.cs
+++ Assets/Scripts/MaterialWarp.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MaterialWarp : MonoBehaviour
@@ -8,112 +7,37 @@ public class MaterialWarp : MonoBehaviour
     private readonly int _backBendID = Shader.PropertyToID("_BackBend");
 
     [SerializeField]
-    private Material _warpMaterial;
+    private Material[] _warpMaterials;
 
     [SerializeField]
     [Range(-1, 1)]
     private float _sidewaysBend = 0f;
+    private float _currentSideBend;
 
     [SerializeField]
     [Range(-1, 1)]
     private float _backBend = 0f;
-
-    [SerializeField]
-    private List<Material> _warpedMaterialsList = new List<Material>();
+    private float _currentBackBend;
 
     private void Update()
     {
-        if (_warpedMaterialsList != null)
+        if (IsBendValueWasChanged())
         {
-            for (int i = 0; i < _warpedMaterialsList.Count; i++)
+            Debug.Log("is changed");
+            for (int i = 0; i < _warpMaterials.Length; i++)
             {
-                SetSidewayBendInMaterial(_warpedMaterialsList[i], _sidewaysBend);
-                SetBackBendInMaterial(_warpedMaterialsList[i], _backBend);
+                _warpMaterials[i].SetFloat(_sideBendID, _sidewaysBend * 0.01f);
+                _warpMaterials[i].SetFloat(_backBendID, _backBend * 0.01f);
             }
+            _currentSideBend = _sidewaysBend;
+            _currentBackBend = _backBend;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private bool IsBendValueWasChanged()
     {
-        other.TryGetComponent<MeshRenderer>(out var meshRenderer);
-        if (meshRenderer == null)
-        {
-            var meshRenderers = other.GetComponentsInChildren<MeshRenderer>();
-            for (int i = 0; i < meshRenderers.Length; i++)
-            {
-                meshRenderer = meshRenderers[i];
-                foreach (var material in meshRenderer.materials)
-                {
-                    Shader outlineShader = Shader.Find("Shader Graphs/Outline");
-                    bool isShaderOutline =
-                            material.shader == outlineShader;
-
-                    if (material.parent == null && !isShaderOutline)
-                    {
-                        material.parent = _warpMaterial;
-                    }
-                    _warpedMaterialsList.Add(material);
-                }
-            }
-        }
-
-        foreach (var material in meshRenderer.materials)
-        {
-            Shader outlineShader = Shader.Find("Shader Graphs/Outline");
-            bool isShaderOutline =
-                    material.shader == outlineShader;
-
-            if (material.parent == null && !isShaderOutline)
-            {
-                material.parent = _warpMaterial;
-            }
-            _warpedMaterialsList.Add(material);
-        }
+        return _currentSideBend != _sidewaysBend ||
+                        _currentBackBend != _backBend;
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        other.TryGetComponent<MeshRenderer>(out var meshRenderer);
-        if (meshRenderer != null)
-        {
-            var material = meshRenderer.material;
-            if (_warpedMaterialsList.Contains(material))
-            {
-                ResetProperties(material);
-                _warpedMaterialsList.Remove(material);
-            }
-        }
-        else
-        {
-            var meshRenderers = other.GetComponentsInChildren<MeshRenderer>();
-            foreach (var mr in meshRenderers)
-            {
-                var materials = mr.materials;
-                foreach (var material in materials)
-                {
-                    if (_warpedMaterialsList.Contains(material))
-                    {
-                        ResetProperties(material);
-                        _warpedMaterialsList.Remove(material);
-                    }
-                }
-            }
-        }
-    }
-
-    private void SetSidewayBendInMaterial(Material material, float value)
-    {
-        material.SetFloat(_sideBendID, value * 0.01f);
-    }
-
-    private void SetBackBendInMaterial(Material material, float value)
-    {
-        material.SetFloat(_backBendID, value * 0.01f);
-    }
-
-    private void ResetProperties(Material material)
-    {
-        SetSidewayBendInMaterial(material, 0);
-        SetBackBendInMaterial(material, 0);
-    }
 }
