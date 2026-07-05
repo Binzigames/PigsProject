d916a5730f2591fc5755ba2d2b630cae20adf082
Serhii <143748403+SerhiiOvD@users.noreply.github.com>
2026-07-05 17:23:41 +0200

    settings

--- Assets/Scripts/UI/Views/SettingsView.cs
+++ Assets/Scripts/UI/Views/SettingsView.cs
@@ -7,6 +7,9 @@ public class SettingsView : UIView
     private const string SOUND_TOGGLE = "SoundToggle";
     private const string MUSIC_TOGGLE = "MusicToggle";
     private const string HAPTIC_TOGGLE = "HapticToggle";
+    private const string SOUND_IMG_CHECKER = "sound-checker";
+    private const string MUSIC_IMG_CHECKER = "music-checker";
+    private const string HAPTIC_IMG_CHECKER = "haptic-checker";
     private const string GAME_VERSION = "GameVersion";
 
     private VisualElement _exitSettingsButton;
@@ -16,9 +19,17 @@ public class SettingsView : UIView
     private Toggle _musicToggle;
     private Toggle _hapticToggle;
 
+    private Image _soundChecker;
+    private Image _musicChecker;
+    private Image _hapticChecker;
+
     public SettingsView(VisualElement root) : base(root)
     {
         _gameVersion.text = Application.version;
+
+        _hapticChecker.visible = _hapticToggle.value;
+        _musicChecker.visible = _musicToggle.value;
+        _soundChecker.visible = _soundToggle.value;
     }
 
     protected override void SetVisualElements()
@@ -28,6 +39,10 @@ public class SettingsView : UIView
         _soundToggle = _root.Q<Toggle>(SOUND_TOGGLE);
         _musicToggle = _root.Q<Toggle>(MUSIC_TOGGLE);
         _hapticToggle = _root.Q<Toggle>(HAPTIC_TOGGLE);
+
+        _soundChecker = _root.Q<Image>(SOUND_IMG_CHECKER);
+        _musicChecker = _root.Q<Image>(MUSIC_IMG_CHECKER);
+        _hapticChecker = _root.Q<Image>(HAPTIC_IMG_CHECKER);
 
         _gameVersion = _root.Q<Label>(GAME_VERSION);
     }
@@ -44,16 +59,19 @@ public class SettingsView : UIView
     private void OnHapticToggle(ClickEvent evt)
     {
         SettingsEvents.OnHapticToggle?.Invoke(_hapticToggle.value);
+        _hapticChecker.visible = _hapticToggle.value;
     }
 
     private void OnMusicToggle(ClickEvent evt)
     {
         SettingsEvents.OnMusicToggle?.Invoke(_musicToggle.value);
+        _musicChecker.visible = _musicToggle.value;
     }
 
     private void OnSoundToggle(ClickEvent evt)
     {
         SettingsEvents.OnSoundToggle?.Invoke(_soundToggle.value);
+        _soundChecker.visible = _soundToggle.value;
     }
 
     private void OnExitSettings(ClickEvent click)
