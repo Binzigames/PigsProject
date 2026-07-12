b179ec61f9603ed8d5084d16409b383221cfa659
Serhii <143748403+SerhiiOvD@users.noreply.github.com>
2026-07-12 15:17:46 +0200

    hud anim

--- Assets/Scripts/UI/Views/HUDView.cs
+++ Assets/Scripts/UI/Views/HUDView.cs
@@ -1,14 +1,23 @@
+using DG.Tweening;
 using UnityEngine.UIElements;
 
 public class HUDView : UIView
 {
+    private const string CARROT_IMAGE = "carrot-image";
     private const string PAUSE_BUTTON = "pause-button";
     private const string SCORE_LABLE = "score-label";
     private const string CURRENCY_LABLE = "currency-label";
 
+    private const float TARGET_SCALE = 1.1f;
+    private const float DEFAULT_SCALE = 1f;
+    private const float ANIM_DURATION = 0.05f;
+
     private Button _pauseButton;
     private Label _scoreLable;
     private Label _currencyLable;
+    private Image _carrotImage;
+
+    private Tween _scaleAnim;
 
     public HUDView(VisualElement root) : base(root)
     {
@@ -21,6 +30,7 @@ public class HUDView : UIView
         _pauseButton = _root.Q<Button>(PAUSE_BUTTON);
         _scoreLable = _root.Q<Label>(SCORE_LABLE);
         _currencyLable = _root.Q<Label>(CURRENCY_LABLE);
+        _carrotImage = _root.Q<Image>(CARROT_IMAGE);
     }
 
     protected override void RegisterButtonCallbacks()
@@ -41,6 +51,18 @@ public class HUDView : UIView
     private void UpdateCurrency(int value)
     {
         _currencyLable.text = value.ToString();
+        ScaleImageSequence(_carrotImage);
     }
 
+    private Sequence ScaleImageSequence(Image image)
+    {
+        Sequence sequence = DOTween.Sequence();
+        return sequence.Append(ScaleCarrotAnim(TARGET_SCALE, ANIM_DURATION))
+                            .Append(ScaleCarrotAnim(DEFAULT_SCALE, ANIM_DURATION));
+    }
+
+    private Tween ScaleCarrotAnim(float scale, float duration)
+    {
+        return _carrotImage.DOScale(scale, duration);
+    }
 }
